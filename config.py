b961b597232961cdce4434c1bcb4f078d88ad5f3
DigitalGalaxyHQ <sainiabhi310@gmail.com>
2025-08-07 07:37:58 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TMDB_API_KEY = os.getenv('TMDB_API_KEY')
-TMDB_BASE_URL = 'https://api.themoviedb.org/3'
-TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original'
