e390cf7a5aadfbc760207090d10eae4a58d16ffe
DigitalGalaxyHQ <sainiabhi310@gmail.com>
2025-10-21 17:18:01 +0530

    Delete imghdr.py

--- imghdr.py
+++
@@ -1,6 +0,0 @@
-import mimetypes, os
-def what(file, h=None):
-    mime = mimetypes.guess_type(file)[0]
-    if mime and mime.startswith("image/"):
-        return mime.split("/")[1]
-    return None
