e1d29fd12432cb6fccf4661b90b45c527d331916
DigitalGalaxyHQ <sainiabhi310@gmail.com>
2025-10-21 17:06:45 +0530

    Create imghdr.py

---
+++ imghdr.py
@@ -0,0 +1,6 @@
+import mimetypes, os
+def what(file, h=None):
+    mime = mimetypes.guess_type(file)[0]
+    if mime and mime.startswith("image/"):
+        return mime.split("/")[1]
+    return None
